40c431490105e27b15a44850335ca6673f6606bd
Misagar16 <sagarmisal3185@gmail.com>
2024-11-08 13:29:02 +0530

    Add Auto-text-effect-animator project

---
+++ Auto-text-effect-animator/script.js
@@ -0,0 +1,20 @@
+const containerEl = document.querySelector(".container");
+const careers = ["YOUTUBER","INSTRUCTOR","WEB DEVOLOPER","FREELANCER"]
+let careerInx = 0;
+let characteridx = 0;
+
+updatetxt();
+
+function updatetxt(){
+    characteridx++;
+    containerEl.innerHTML = `<h1>I AM ${careers[careerInx].slice(0,1) === "I" ? "AN": "A"} ${careers[careerInx].slice(0,characteridx)}</h1>`;
+
+    if(characteridx === careers[careerInx].length){
+        careerInx++;
+        characteridx = 0;
+    }
+    if(careerInx === careers.length){
+        careerInx =0;
+    }
+    setTimeout(updatetxt,400);
+}
